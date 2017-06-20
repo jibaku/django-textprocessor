460fddb99f46c5f5a4a0e98edbd48c1328c77f6b
Fabien Schwob <fabien.schwob@89grad.ch>
2017-06-20 09:47:32 +0200

    updating version number to restart dev.

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="textprocessor",
-    version="0.1.1",
+    version="0.2.0",
     author="Fabien Schwob",
     license="BSD",
     url="http://github.com/jibaku/django-textprocessor",
